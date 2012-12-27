b48b27887c94d3c374ea79bb6ad23e9fd4e7b32a
Andrew Childs <ac@glomerate.com>
2012-12-27 13:35:42 -0500

    AC: Update search to fire on enter key

--- app/public/js/tt.search.js
+++ app/public/js/tt.search.js
@@ -2,6 +2,10 @@ TT.Search = (function () {
 
   var pub = {};
 
+  function enterKeyPressed(e) {
+    return e.which === 13;
+  }
+
   pub.addSearchTag = function (term) {
     TT.addFilter({
       name: term,
@@ -17,14 +21,14 @@ TT.Search = (function () {
     var search = $('#search input');
     search.blur(function () {
       search.val('');
-    }).keyup(function () {
-      clearTimeout(timeout);
-      timeout = setTimeout(function () {
+    }).keyup(function (e) {
+      if (enterKeyPressed(e)) {
         var term = $.trim(search.val().toLowerCase());
         if (term) {
           pub.addSearchTag(term);
         }
-      }, 500);
+        search.val('');
+      }
     });
   };
 
