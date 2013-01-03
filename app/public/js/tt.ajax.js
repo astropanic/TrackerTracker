3371ba9bc03898f395b10af84b2e2032d57e4d82
Andrew Childs <ac@glomerate.com>
2013-01-02 23:07:29 -0500

    AC: Add ajaxRunning UI timeout

--- app/public/js/tt.ajax.js
+++ app/public/js/tt.ajax.js
@@ -2,20 +2,24 @@ var TT = TT || {};
 TT.Ajax = (function () {
 
   var pub = {};
-  var activeServerRequests = 0;
+  var serverRequests = 0;
+  var serverResponses = 0;
+  var timeouts = [];
 
   // ajax UI state
 
   pub.start = function () {
-    activeServerRequests++;
+    serverRequests++;
     $('body').addClass('ajaxRunning');
+    timeouts[serverRequests] = setTimeout(pub.end, 10000);
   };
 
   pub.end = function () {
-    activeServerRequests--;
-    if (!activeServerRequests) {
+    serverResponses++;
+    if (serverRequests === serverResponses) {
       $('body').removeClass('ajaxRunning');
     }
+    clearTimeout(timeouts[serverResponses]);
   };
 
   return pub;
