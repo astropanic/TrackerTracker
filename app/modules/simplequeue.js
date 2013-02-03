21960b0dc58a4f0f57bf9cdbf61c847c7cad4c98
Andrew Childs <ac@glomerate.com>
2013-02-03 16:12:26 -0500

    AC: Add custom async queue management library, since existing libraries didn't fit the bill

---
+++ app/modules/simplequeue.js
@@ -0,0 +1,32 @@
+var queue = [];
+var isRunning = false;
+
+exports.push = function () {
+  queue.push(Array.prototype.slice.call(arguments, 0));
+};
+
+exports.start = function () {
+  if (!isRunning) {
+    exports.run();
+  }
+};
+
+exports.run = function () {
+  if (queue.length > 0) {
+    isRunning = true;
+    var current_args = queue.shift();
+    var current_fn = current_args.shift();
+    current_fn.apply(this, current_args);
+  }
+};
+
+exports.next = function () {
+  if (queue.length > 0) {
+    exports.run();
+  } else {
+    isRunning = false;
+    exports.finished();
+  }
+};
+
+exports.finished = function () {};
